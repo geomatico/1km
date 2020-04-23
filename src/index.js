8334459e2f66e4416ac414681bbe51b72bcfe92e
Francisco Pérez Sampayo <fpsampayo@gmail.com>
2020-04-23 17:54:33 +0200

    Added Buffer on click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css'
-
+import 'mapbox-gl/dist/mapbox-gl.css';
+import turfCircle from '@turf/circle';
+import {point as turfPoint} from '@turf/helpers'
 
 var map = new mapboxgl.Map({
     container: 'map',
@@ -10,6 +11,64 @@ var map = new mapboxgl.Map({
     bearing: 0
 });
 
+map.on('load', function () {
+    map.addSource('buffer', {
+        type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            features: []
+        }
+    });
+    map.addSource('buffer_center', {
+        type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            features: []
+        }
+    });
+
+    map.addLayer({
+        'id': 'buffer',
+        'type': 'fill',
+        'source': 'buffer',
+        'paint': {
+            'fill-color': '#973572',
+            'fill-opacity': 0.3
+        }
+    });
+
+    map.addLayer({
+        'id': 'buffer_center',
+        'type': 'circle',
+        'source': 'buffer_center',
+        'paint': {
+            'circle-color': '#973572',
+            'circle-radius': 5,
+            'circle-stroke-color': '#FFF',
+            'circle-stroke-width': 2
+        }
+    });
+});
+
 map.addControl(new mapboxgl.NavigationControl());
 map.addControl(new mapboxgl.ScaleControl({position: 'bottom-right'}));
+
+map.on('click', function f(e) {
+
+    const center = turfPoint([e.lngLat.lng, e.lngLat.lat]);
+    const radius = 1;
+    const options = {steps: 100, units: 'kilometers', properties: {foo: 'bar'}};
+    const circle = turfCircle(center, radius, options);
+
+    map.getSource('buffer_center').setData(center);
+    map.getSource('buffer').setData(circle);
+
+    const bounds = circle.geometry.coordinates[0].reduce(function (bounds, coord) {
+        return bounds.extend(coord);
+    }, new mapboxgl.LngLatBounds());
+
+    map.fitBounds(bounds, {padding: 25});
+});
+
+
 
