dec9557c9a6ddf08576079710195acf6fbb39114
Francisco Pérez Sampayo <fpsampayo@gmail.com>
2020-04-23 18:04:08 +0200

    Ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,12 @@ module.exports = {
                     'css-loader',
                 ],
             },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
         ],
     },
 };
