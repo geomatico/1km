f67f917db4221ce5a2e294ca7725848a3ac0f349
Oscar Fonts <oscar.fonts@geomati.co>
2020-04-24 15:51:36 +0200

    Cloudfront mediante

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvbWF0aWNvIiwiYSI6ImNrOWQyYnZnMzA0Y2gzZnBsajkzdTV1eW0ifQ.Sdirap8fgylWmhv6iBxUOA'
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'https://geoserveis.icgc.cat/contextmaps/osm-bright.json',
+    style: 'img/osm-bright-icgc-cloudfront.json',
     center: [-3.69, 40.41],
     zoom: 6,
     bearing: 0
