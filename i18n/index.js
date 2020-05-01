3b6474c3f86ee7f7076db655d3d454e96a565e3e
Oscar Fonts <oscar.fonts@geomati.co>
2020-05-01 18:32:15 +0200

    i18next package & config

---
+++ i18n/index.js
@@ -0,0 +1,37 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import es from './es.json';
+import ca from './ca.json';
+import en from './en.json';
+
+i18n.use(LanguageDetector)
+    .init({
+        detection: {
+            order: ['querystring', 'navigator'],
+            lookupQuerystring: 'lang',
+            checkWhitelist: true
+        },
+        resources: {
+            es: {
+                translation: es
+            },
+            ca: {
+                translation: ca
+            },
+            en: {
+                translation: en
+            },
+        },
+        load: 'languageOnly',
+        whitelist: ['es', 'ca', 'en'],
+        fallbackLng: 'es',
+        debug: false,
+        keySeparator: false,
+        interpolation: {
+            escapeValue: false,
+            formatSeparator: ','
+        }
+    });
+
+export default i18n;
